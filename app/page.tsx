d8a8e6e6e2398f739abfbe938fa13fdb1dbcb7ac
MaverickMaddy11 <chopramadhav2004@gmail.com>
2025-06-12 20:02:30 +0530

    third commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <div>Hello there this is next app made for eployment</div>;
+  return (
+    <div>
+      Hello there this is next app made for eployment this is first
+      implementation of ci/cd pipline
+    </div>
+  );
 }
